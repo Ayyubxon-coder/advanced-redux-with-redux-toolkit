667bfaeb3776a98bfa79c0c0dfca3c8d35999f4d
ayyubxon-coder <ayyubxonkamoldinovcoder@gmail.com>
2023-04-27 11:13:20 +0500

    slice has been added

--- simple-task/src/features/tasks-slice.ts
+++ simple-task/src/features/tasks-slice.ts
@@ -1,1 +1,22 @@
-const initialState = [];
+import { createSlice } from '@reduxjs/toolkit';
+import { PayloadAction } from '@reduxjs/toolkit/dist/createAction';
+
+export type TaskState = {
+  entities: Task[];
+};
+
+const initialState: TaskState = {
+  entities: [],
+};
+const tasksSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    addTask: (state, action: PayloadAction<Task>) => {
+      state.entities.unshift(action.payload);
+    },
+    removeTask: (state, action) => {
+      //
+    },
+  },
+});
